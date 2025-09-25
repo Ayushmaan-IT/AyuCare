ed68da74d20386d93fc4d6dd15982a972449941c
Ayushmaan Mishra <mishraayushmaan3@gmail.com>
2025-09-25 10:33:26 +0530

    Fix CORS: allow frontend URL

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,13 @@ connectDB();
 connectCloudinary();
 // middlewares
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://ayucare-frontend.onrender.com"], // allow frontend
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 //api endpoints
 app.use("/api/admin", adminRouter);
